5121d613f3086db0dea14f1641afa683a6f259ae
Teodor <raduteodorgeorge0@gmail.com>
2019-04-25 19:35:30 +0300

    Initial commit

---
+++ header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int removeit()
+{
+    return 1;
+}
+
+
+#endif // HEADER_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "header.h"
+
+using namespace std;
+
+struct coord
+{
+    int x;
+    int y;
+};
+
+class Hunter
+{
+private:
+    coord position;
+public:
+    coord getPosition()
+    {
+
+    }
+
+
+};
+
+
+
+
+int main()
+{
+
+    return 0;
+}
